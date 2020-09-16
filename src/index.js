af52c191a5946679b45f02f7007dee289bafe11c
Vladimir <uptonpark82@gmail.com>
2020-09-16 16:07:55 +0300

    second step

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import program from 'commander';
 
 const getConfiguration = () => program
     .version('1.0.0')
+    .arguments('<filepath1> <filepath2>')
     .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]' , 'output format')
     .helpOption('-h, --help' ,  'output usage information' ) 
     .addHelpCommand(false)
     .parse(process.argv);

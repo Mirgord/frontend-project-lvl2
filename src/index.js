dd9ce1a42448f66c6e371ca7ac335e143406993c
Vladimir <uptonpark82@gmail.com>
2020-10-13 14:03:28 +0300

    refactoring step 7

--- src/index.js
+++ src/index.js
@@ -20,23 +20,41 @@ const readFile = (filename) => {
   const data = fs.readFileSync(fullPath).toString();
   return parse(data, format);
 };
-
-const genDiff = (path1, path2) => {
-  const data1 = readFile(path1);
-  const data2 = readFile(path2);
-  const keys = _.union(_.keys(_.cloneDeep(data1)), _.keys(_.cloneDeep(data2))).sort();
-  const diff = keys.reduce((acc, key) => {
-    if (_.has(data1, key) && _.has(data2, key) && data1[key] === data2[key]) {
-      return `${acc} \n  ${key}: ${data1[key]}`;
-    } if (_.has(data1, key) && _.has(data2, key) && data1[key] !== data2[key]) {
-      return `${acc} \n- ${key}: ${data1[key]}\n+ ${key}: ${data2[key]}`;
-    } if (_.has(data1, key) && (!_.has(data2, key))) {
-      return `${acc} \n- ${key}: ${data1[key]}`;
+const formator = (data) => {
+  const a = data.reduce((acc, child) => {
+    const {
+      type, key, value1, value2,
+    } = child;
+    if (type === 'unchanged') {
+      return `${acc} \n  ${key}: ${value1}`;
+    } if (type === 'changed') {
+      return `${acc} \n- ${key}: ${value1}\n+ ${key}: ${value2}`;
+    } if (type === 'removed') {
+      return `${acc} \n- ${key}: ${value1}`;
     }
-    return `${acc} \n+ ${key}: ${data2[key]}`;
+    return `${acc} \n+ ${key}: ${value1}`;
   }, '');
-  console.log(`{${diff}\n}`);
-  return `{${diff}\n}`;
+  console.log(`{${a}\n}`);
+  return `{${a}\n}`;
+};
+
+const genDiff = (file1, file2) => {
+  const data1 = readFile(file1);
+  const data2 = readFile(file2);
+  const keys = _.union(_.keys(_.clone(data1)), _.keys(_.clone(data2))).sort();
+  const diff = keys.map((key) => {
+    if (_.has(data1, key) && _.has(data2, key) && data1[key] === data2[key]) {
+      return { type: 'unchanged', key: `${[key]}`, value1: data1[key] };
+    } if (_.has(data1, key) && _.has(data2, key) && data1[key] !== data2[key]) {
+      return {
+        type: 'changed', key: `${[key]}`, value1: data1[key], value2: data2[key],
+      };
+    } if (_.has(data1, key) && (!_.has(data2, key))) {
+      return { type: 'removed', key: `${[key]}`, value1: data1[key] };
+    }
+    return { type: 'added', key: `${[key]}`, value1: data2[key] };
+  });
+  return formator(diff);
 };
 
 export default genDiff;

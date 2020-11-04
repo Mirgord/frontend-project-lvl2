ebc1dbb172adb2cfadf4864fb63bdeff7d336a32
Vladimir <uptonpark82@gmail.com>
2020-11-04 21:15:28 +0300

    fix first check comments, refactoring test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,8 +8,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').toString();
-test.each(['.yml', '.json'])('diff_files', (format) => {
-  expect(getFormatter(genDiff(`file1${format}`, `file2${format}`), 'stylish')).toEqual(readFile('expected_stylish'));
-  expect(getFormatter(genDiff(`file1${format}`, `file2${format}`), 'json')).toEqual(readFile('expected_json'));
-  expect(getFormatter(genDiff(`file1${format}`, `file2${format}`), 'plain')).toEqual(readFile('expected_plain'));
+test.each(['yml', 'json'])('diff_files', (format) => {
+  expect(getFormatter(genDiff(`file1.${format}`, `file2.${format}`), 'stylish')).toEqual(readFile('expected_stylish'));
+  expect(getFormatter(genDiff(`file1.${format}`, `file2.${format}`), 'json')).toEqual(readFile('expected_json'));
+  expect(getFormatter(genDiff(`file1.${format}`, `file2.${format}`), 'plain')).toEqual(readFile('expected_plain'));
 });

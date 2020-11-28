7cfe52862325cd555d023fbc8523f21b96930064
Vladimir <uptonpark82@gmail.com>
2020-11-28 19:50:17 +0300

    fix fifth check comments

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,21 +1,28 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import buildDiff from '../src/index.js';
 import readFile from '../src/utils.js';
 
-const makeFixturePath = (filename) => `__tests__/__fixtures__/${filename}`;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const makeFixturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
+const expectedStylish = readFile(makeFixturePath('expected_stylish'));
+const expectedJson = readFile(makeFixturePath('expected_json'));
+const expectedPlain = readFile(makeFixturePath('expected_plain'));
 
 test.each(['yml', 'json'])('diff_In_Files', (format) => {
   expect(buildDiff(makeFixturePath(`file1.${format}`), makeFixturePath(`file2.${format}`), 'stylish'))
-    .toEqual(readFile(makeFixturePath('expected_stylish')));
+    .toEqual(expectedStylish);
   expect(buildDiff(makeFixturePath(`file1.${format}`), makeFixturePath(`file2.${format}`), 'json'))
-    .toEqual(readFile(makeFixturePath('expected_json')));
+    .toEqual(expectedJson);
   expect(buildDiff(makeFixturePath(`file1.${format}`), makeFixturePath(`file2.${format}`), 'plain'))
-    .toEqual(readFile(makeFixturePath('expected_plain')));
+    .toEqual(expectedPlain);
 });
 test.each(['yml', 'json'])('difference_In_Files_With_Default_Format', (format) => {
   expect(buildDiff(makeFixturePath(`file1.${format}`), makeFixturePath(`file2.${format}`)))
-    .toEqual(readFile(makeFixturePath('expected_stylish')));
+    .toEqual(expectedStylish);
   expect(buildDiff(makeFixturePath(`file1.${format}`), makeFixturePath(`file2.${format}`)))
-    .toEqual(readFile(makeFixturePath('expected_stylish')));
+    .toEqual(expectedStylish);
   expect(buildDiff(makeFixturePath(`file1.${format}`), makeFixturePath(`file2.${format}`)))
-    .toEqual(readFile(makeFixturePath('expected_stylish')));
+    .toEqual(expectedStylish);
 });

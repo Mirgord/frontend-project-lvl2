69ecb986c75d18852eaead8c341d794400e8dd08
Vladimir <uptonpark82@gmail.com>
2020-12-05 17:20:28 +0300

    fix sixth check comments

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,23 +1,23 @@
 import _ from 'lodash';
 
 const step = 1;
-const indent = 4;
-
-const getTab = (depth, gap = ' ') => _.padStart(gap, depth * indent);
+const offset = 4;
+const prefixOffset = 2;
 
 const stringify = (data, depth) => {
   if (!_.isPlainObject(data)) {
     return data;
   }
-  const tab = getTab(depth);
-  const offset = getTab(step);
+  const indentSize = depth * offset;
+  const keyIndent = ' '.repeat(indentSize + offset);
+  const bracketIndent = ' '.repeat(indentSize);
   const keys = _.keys(data);
   const result = keys.map((key) => {
-    const prefix = `${tab}${offset}${key}`;
+    const prefix = `${keyIndent}${key}`;
     const suffix = stringify(data[key], depth + step);
     return `${prefix}: ${suffix}`;
   });
-  return `{\n${result.join('\n')}\n${tab}}`;
+  return `{\n${result}\n${bracketIndent}}`;
 };
 
 const stylish = (tree) => {
@@ -26,25 +26,27 @@ const stylish = (tree) => {
       const {
         type, key, children, value, value1, value2,
       } = item;
-      const tab = getTab(depth);
+      const indentSize = depth * offset;
+      const keyIndent = ' '.repeat(indentSize - prefixOffset);
+      const bracketIndent = ' '.repeat(indentSize);
       switch (type) {
         case 'unchanged':
-          return `${getTab(depth, '  ')}${key}: ${stringify(value, depth)}`;
+          return `${keyIndent}  ${key}: ${stringify(value, depth)}`;
         case 'nested':
-          return `${getTab(depth, '  ')}${key}: {\n${iter(children, depth + step)}\n${tab}}`;
+          return `${keyIndent}  ${key}: {\n${iter(children, depth + step)}\n${bracketIndent}}`;
         case 'changed':
-          return `${getTab(depth, '- ')}${key}: ${stringify(value1, depth)}\n${getTab(depth, '+ ')}${key}: ${stringify(value2, depth)}`;
+          return `${keyIndent}- ${key}: ${stringify(value1, depth)}\n${keyIndent}+ ${key}: ${stringify(value2, depth)}`;
         case 'removed':
-          return `${getTab(depth, '- ')}${key}: ${stringify(value, depth)}`;
+          return `${keyIndent}- ${key}: ${stringify(value, depth)}`;
         case 'added':
-          return `${getTab(depth, '+ ')}${key}: ${stringify(value, depth)}`;
+          return `${keyIndent}+ ${key}: ${stringify(value, depth)}`;
         default:
           throw new Error(`Unknown type: '${type}'!`);
       }
     });
-    return result.join('\n');
+    return result;
   };
   const result = iter(tree);
-  return `{\n${result}\n}`;
+  return `{\n${result.join().split(',').join('\n')}\n}`;
 };
 export default stylish;
